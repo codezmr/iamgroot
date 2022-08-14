cf1fbed2cf2878055d23951788ef3c25efc21590
Mohammad Zamiruddin <mdzamiruddin.zmr@gmail.com>
2022-08-14 17:14:46 +0530

    codezmr
    First Commit

---
+++ js/audioPlay.js
@@ -0,0 +1,25 @@
+
+
+// const fontsNameArr = ["Gamros-regular.otf","Medrual-regular.otf"];
+ 
+// function changeGroot(fontsNameArr){
+
+//   var fonts=  document.getElementById("groot-font-text").style.fontFamily = "/fonts/OTF/"+fontsNameArr[1];
+//     console.log(fonts);
+// }
+// function writeTxt(){
+//     const fs = require('fs');
+//     let data = "Learning how to write in a file."
+      
+//     fs.appendFileSync('style.css', data)
+// }
+
+
+function playGroot() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    
+}
+
+
+
